14856bce7afef249ab242840f3fef69a21874744
Vdmrl <vvluzin2004@gmail.com>
2025-03-17 02:14:14 +0300

    fix: alembic init fixed for docker-compose

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,6 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from dotenv import load_dotenv
+
+load_dotenv("../.env.prod")
+
+from db.config import settings
+from db.base import Base
+from db.models import user # noqa
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,12 +26,12 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL_asyncpg + "?async_fallback=True")
 
 
 def run_migrations_offline() -> None:
