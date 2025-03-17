31ca260ccf82205c588778b4b3d8563f2109746e
Vdmrl <vvluzin2004@gmail.com>
2025-03-17 17:47:59 +0300

    feat: authentication router

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from api.routes import detector
+from api.routes import auth
 
 
 api_router = APIRouter()
 api_router.include_router(detector.router)
+api_router.include_router(auth.router)
---
+++ backend/api/routes/auth.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, File, UploadFile, status, HTTPException
+from api.dependencies.authentication.fastapi_users import fastapi_users
+from api.dependencies.authentication.backend import authentication_backend
+from logger import logger
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+router.include_router(
+    router=fastapi_users.get_auth_router(authentication_backend),
+)
