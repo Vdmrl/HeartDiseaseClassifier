908a4514d314c6c800e19d21da8fad29032448db
Vdmrl <vvluzin2004@gmail.com>
2025-03-17 17:24:49 +0300

    fix: user manager dependencies

--- backend/api/dependencies/user_managers.py
+++ backend/api/dependencies/user_managers.py
@@ -1,6 +1,9 @@
-from fastapi import Depends
 from core.auth.user_manager import UserManager
 from .users import get_user_db
+from typing import Annotated
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 
-async def get_user_manager(user_db=Depends(get_user_db)):
+
+async def get_user_manager(user_db: Annotated[SQLAlchemyUserDatabase, Depends(get_user_db)]):
     yield UserManager(user_db)
