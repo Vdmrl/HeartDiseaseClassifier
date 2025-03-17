0cdff44b00948fc1876344a3bdab7a5816500c01
Vdmrl <vvluzin2004@gmail.com>
2025-03-17 21:05:52 +0300

    feat: added HTTPBearer dependency

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from api.routes import detector
 from api.routes import auth
 from api.routes import users
 
+http_bearer = HTTPBearer(auto_error=False)
 
-api_router = APIRouter()
+api_router = APIRouter(
+    dependencies=[Depends(http_bearer)]
+)
 api_router.include_router(detector.router)
 api_router.include_router(auth.router)
 api_router.include_router(users.router)
