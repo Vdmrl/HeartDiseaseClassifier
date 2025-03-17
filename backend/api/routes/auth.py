94c66a2d6b7be85cbd22ec899a159023470a008a
Vdmrl <vvluzin2004@gmail.com>
2025-03-17 17:58:20 +0300

    feat: added register router

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter, File, UploadFile, status, HTTPException
 from api.dependencies.authentication.fastapi_users import fastapi_users
 from api.dependencies.authentication.backend import authentication_backend
-from logger import logger
+from schemas.user import UserRead, UserCreate
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
+# /login
+# /logout
 router.include_router(
     router=fastapi_users.get_auth_router(authentication_backend),
+)
+
+# /register
+router.include_router(
+    router=fastapi_users.get_register_router(UserRead, UserCreate)
 )
