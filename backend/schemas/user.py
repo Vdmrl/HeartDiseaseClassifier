928132755abb8b4c6738848208d225b2b6f6bca2
Vdmrl <vvluzin2004@gmail.com>
2025-03-17 17:29:52 +0300

    fix: users schemas add missing field

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -2,12 +2,15 @@ from fastapi_users import schemas
 
 
 class UserRead(schemas.BaseUser[int]):
+    age: int
     pass
 
 
 class UserCreate(schemas.BaseUserCreate):
+    age: int
     pass
 
 
 class UserUpdate(schemas.BaseUserUpdate):
+    age: int
     pass
